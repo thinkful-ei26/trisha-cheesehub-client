edc4c963604a288b890c6cfc551c8242078e92fb
kronicle114 <trisha.aguinaldo@gmail.com>
2018-12-21 02:54:05 -0500

    async actions, reducers, and store working

---
+++ src/actions/cheese.js
@@ -0,0 +1,33 @@
+import { API_BASE_URL } from '../config';
+
+export const FETCH_CHEESE_REQUEST = 'FETCH_CHEESE_REQUEST';
+export const fetchCheesesRequest = () => ({
+  type: FETCH_CHEESE_REQUEST,
+})
+
+export const FETCH_CHEESE_SUCCESS = 'FETCH_CHEESE_SUCCESS';
+export const fetchCheesesSuccess = (cheeses) => ({
+  type: FETCH_CHEESE_SUCCESS,
+  cheeses
+})
+
+export const FETCH_CHEESE_ERROR = 'FETCH_CHEESE_ERROR';
+export const fetchCheesesError = (error) => ({
+  type: FETCH_CHEESE_ERROR,
+  error
+})
+
+export const fetchCheeses = () => dispatch => {
+  fetch(`${API_BASE_URL}/cheeses`)
+    .then(res => {
+      if(!res.ok) {
+        return Promise.reject(res.statusText);
+      }
+      return res.json()
+    })
+    .then( cheese => { 
+      dispatch(fetchCheesesSuccess(cheese))
+    })
+    .catch( error => { dispatch(fetchCheesesError(error))
+    })
+}
---
+++ src/reducers/cheeseReducer.js
@@ -0,0 +1,32 @@
+// import * as actions from '../actions';
+import { FETCH_CHEESE_REQUEST, FETCH_CHEESE_SUCCESS, FETCH_CHEESE_ERROR } from '../actions/cheese';
+
+const initialState = {
+  cheeses: [],
+  loading: false,
+  error: null
+}
+
+export const cheeseReducer = (state=initialState, action) => {
+  switch (action.type) {
+    case FETCH_CHEESE_REQUEST : 
+    return Object.assign({}, state, { loading: true })
+
+    case FETCH_CHEESE_SUCCESS :
+    return Object.assign({}, state, { 
+      loading: false, 
+      cheeses: action.cheeses, 
+      error: null
+    })
+
+    case FETCH_CHEESE_ERROR :
+    return Object.assign({}, state, { 
+      loading: true,
+      error: action.error
+    })
+
+    default: return state
+  }
+}
+
+export default cheeseReducer;
